9230af125bff31e83747e3f659d2aa2b3ffabaad
Kantamas <pokkam55@gmail.com>
2023-08-29 23:18:57 +0700

    Fix: button Home and add tag p in Header

--- src/layouts/Default/Header/index.tsx
+++ src/layouts/Default/Header/index.tsx
@@ -10,21 +10,25 @@ const Header = () => {
     <Fragment>
       <header className={clsx(`main-space-x py-4`, `fixed top-0 z-50 w-full bg-orange-500`)}>
         <div className={clsx(`container flex items-center justify-between`)}>
-          <p>Header</p>
+          <Button className={buttonHeaderStyle}>
+            <Link activeClass="active" to="aboutMe" spy={true} smooth={true} offset={-76} duration={500}>
+              <p>Home</p>
+            </Link>
+          </Button>
           <div className={clsx(`space-x-8`)}>
             <Button className={buttonHeaderStyle}>
               <Link activeClass="active" to="aboutMe" spy={true} smooth={true} offset={-76} duration={500}>
-                About
+                <p>About</p>
               </Link>
             </Button>
             <Button className={buttonHeaderStyle}>
               <Link activeClass="active" to="mySkills" spy={true} smooth={true} offset={-76} duration={500}>
-                Skills
+                <p>Skills</p>
               </Link>
             </Button>
             <Button className={buttonHeaderStyle}>
               <Link activeClass="active" to="myProjects" spy={true} smooth={true} offset={-76} duration={500}>
-                Projects
+                <p>Projects</p>
               </Link>
             </Button>
           </div>
